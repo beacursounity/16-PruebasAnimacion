71c9e4b2a8f609693226af042437bf4b88cfcfca
beacursounity <beacursounity@gmail.com>
2018-11-28 08:58:17 +0100

    Initial commit

---
+++ Assets/_GameAssets/Scripts/BonitoScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BonitoScript : MonoBehaviour {
+    public int valor;
+
+	public void Mueve() {
+        GetComponent<Animator>().enabled = false;
+        // HYA QUE LLAMAR AL iNVOKE PARA QUE DE TIEMPO A TERMINAR EL ANIMATION
+        Invoke("Muevete", 0.1f);
+        
+    }
+
+
+    public void Muevete() {
+       this.transform.Translate(Vector3.forward * 2);
+       
+    }
+}
